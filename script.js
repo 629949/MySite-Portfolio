05134df6d9fa5b382ac8bb96ed3f5dd8c514de68
GadObua <gademma54@gmail.com.com>
2025-10-17 13:05:43 +0300

    figured out the responsive format for tailwind

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ scrollbtn.addEventListener("click", function() {
     });
 });
 
+
+
+
+let sendBtn = document.querySelector("#sendMsg");
+sendBtn.addEventListener("click", () => {
+    window.alert("Message sent");
+
+
+
+})
+
+
+//this is the slider 
+const containerWidth = document.getElementById('slider');
+const sliderImage = document.querySelectorAll('#logoSlide');
+
+
+sliderImage.forEach((logo, index) => {
+  let x = index * (logo.offsetWidth + 40); // 40 = space between logos
+  function move() {
+    x -= 1; // speed
+    if (x <= -logo.offsetWidth) {
+      x = containerWidth; // reset to right side
+    }
+    logo.style.transform = `translateX(${x}px)`;
+    requestAnimationFrame(move);
+  }
+  move();
+});
+
+
+//repsonsive nav bar
+  
