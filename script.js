e2cf1ed58c798547a6cd6ee7af22b6d09f96ff3f
GadObua <gademma54@gmail.com.com>
2025-11-10 14:59:18 +0300

    add function to the logo to change color

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+let mylogo = document.querySelector(".logo");
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 350){
+        mylogo.style.color = "white";
+    } else {
+        mylogo.style.color = "black";
+    }
+
+     if(window.scrollY > 350){
+        document.querySelector("nav").style.color = "red";
+    } else {
+        document.querySelector("nav").style.color = "black";
+    }
+}
+)
+
+
+let scrollUp = document.querySelector(".scroll-up");
+
+window.addEventListener("scroll", ()=>{
+
+    let scrollpercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+
+    if(scrollpercentage > 30){
+        scrollUp.style.display = "block";
+    } else {
+        scrollUp.style.display = "none";
+    }
+}
+)
+
+scrollUp.AddEventListner("click", ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
